808099118e295c9823869e8abbf969059dcfabab
Nathan Annareddy <9165881+NathanAnnareddy@users.noreply.github.com>
2021-02-20 14:25:45 -0600

    Update Problem 3.py
    Ok I need to figure out prime numbers

--- Problem 3.py
+++ Problem 3.py
@@ -1,11 +1,24 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143?
 
-import math
+#import math
 
-def primes(num):
-    for i in range(1, math.ceil(math.sqrt(num))):
-        if num % i == 0:
-            print(i)
+#def primes(num):
+#    for i in range(1, math.ceil(math.sqrt(num))):
+#        if num % i == 0:
+#            print(i)
     
-primes(13195)
+#primes(13195)
+
+
+n = 2
+num = int(input())
+while n < num:
+
+
+'''
+2 for loops
+one for the range
+one for number in range % i = 0 
+from 2 to num
+'''
